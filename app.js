046df08e9290f9e50ae3fbd11413f71aea2464ac
maro14 <60809044+maro14@users.noreply.github.com>
2020-07-07 15:24:00 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+import nanoexpress from 'nanoexpress-pro';
+
+
+const app = nanoexpress();
+
+var user_name = ({"name":"Billy", "id": "12"})
+
+app.get('/name', (req, res) => {
+    res.end(JSON.stringify({user_name}));
+});
+
+app.get('/hello', async () => ({name: "Cole"}));
+
+app.listen(4000);
