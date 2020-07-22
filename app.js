f098b214320cf3cd0bbca229838348538fd4d014
maro14 <60809044+maro14@users.noreply.github.com>
2020-07-22 13:42:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ import nanoexpress from 'nanoexpress-pro';
 
 const app = nanoexpress();
 
-var user_name = [{"name": "Billy", "id": 12},
-                {"name": "John", "id": 24}]
+var user_name = [{"name": "Billy", "id": 1},
+                {"name": "John", "id": 2}]
 
 app.get('/name', (req, res) => {
     res.end(JSON.stringify({user_name}));
 });
 
-app.get('/hello',async() => ({name: "Cole"}));
+app.get('/hello',async() => ({name: "Cole", "id": 3}));
 
 app.listen(4000);
